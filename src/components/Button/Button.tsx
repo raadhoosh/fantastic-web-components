b947f88d6e21d6da7fd2671759fa149b0f5c844e
Salman Zare <salman.zare@karteldigital.com>
2018-10-21 16:50:06 +1100

    Changing the script for running the Styleguideist

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 interface IProps {
     /** defining the Button's label */
-    label: string, 
+    label: string,
 }
 
-const Button = (props:IProps) => {
+const Button = (props: IProps) => {
 
     return (
         <button> {props.label} </button>
