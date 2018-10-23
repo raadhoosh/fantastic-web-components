38afd76a5361eb456f41a603f634e4b5cd29bd4a
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-10-23 14:48:17 +0330

    bug fixed

--- src/common/styled.ts
+++ src/common/styled.ts
@@ -1,7 +1,30 @@
 import styled, { CreateStyled } from 'react-emotion';
+interface Alert {
+  fontsize: number | string,
+  radius: number | string,
+  border: number | string,
+  color: number | string,
+}
+interface Badge {
+  fontsize: number | string,
+  borderStyle: string,
+  borderSize: number | string,
+  color: number | string,
+}
 interface Bottom {
+  radius?: number | string,
   fontsize?: number | string,
   border?: number | string,
+}
+interface Input {
+  fontsize: number | string,
+  border: number | string,
+}
+interface Label {
+  fontsize: number | string,
+  borderStyle: string,
+  borderSize: number | string,
+  color: number | string,
 }
 export interface Theme {
   color: {
@@ -26,6 +49,10 @@ export interface Theme {
     family: string,
   },
   bottom: Bottom,
+  badge: Badge,
+  alert: Alert,
+  input: Input,
+  label: Label,
   darken(_color?: string): string,
   hexToRgba(hex: string, a?: number): string,
 }
