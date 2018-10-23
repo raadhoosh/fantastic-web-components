c5a0582dd3a54ce32ab00cbaebf87f6554625b9a
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-10-23 14:48:29 +0330

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './App.css';
 import { ThemeProvider } from 'emotion-theming';
 import mainTheme from './common/mainTheme';
 import Label from './components/Label/Label';
